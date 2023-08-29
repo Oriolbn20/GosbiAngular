7200c0bd5f5121728459bb3a9cc7367a1b8a8a4e
Oriol Bassols <oriolbn20@gmail.com>
2023-08-29 18:25:31 +0200

    Created Basic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PodcastListComponent } from './Project/Components/podcast-list/podcast-list.component';
+import { PodcastDetailsComponent } from './Project/Components/podcast-details/podcast-details.component';
+import { EpisodeDetailsComponent } from './Project/Components/episode-details/episode-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PodcastListComponent,
+    PodcastDetailsComponent,
+    EpisodeDetailsComponent
   ],
   imports: [
     BrowserModule,
