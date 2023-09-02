918d98686b01cd1952c7086f1718dd3f2e4938cf
Oriol Bassols <oriolbn20@gmail.com>
2023-09-02 19:27:12 +0200

    Podcast list with cache

--- src/app/Project/Components/podcast-list/podcast-list.component.ts
+++ src/app/Project/Components/podcast-list/podcast-list.component.ts
@@ -7,36 +7,54 @@ import { Subscription } from 'rxjs';
   templateUrl: './podcast-list.component.html',
   styleUrls: ['./podcast-list.component.scss']
 })
-export class PodcastListComponent implements OnInit, OnDestroy  {
+export class PodcastListComponent implements OnInit, OnDestroy {
 
   podcasts: any;
   searchText: string = '';
   subscription!: Subscription;
 
-  constructor(private apiService: ApiService) {}
-  
+  constructor(private apiService: ApiService) { }
+
   ngOnInit(): void {
-    // Starts a subscription to the api service to get the 100 popular podcasts and saves it into a variable
-    this.subscription = this.apiService.get100Popular().subscribe((response: any) => {
-      this.podcasts = response.feed.entry;
+    // Check if data is in localStorage and not expired
+    const cachedData = localStorage.getItem('podcastData');
+    const expiration = localStorage.getItem('podcastDataExpiration');
+
+    if (cachedData && expiration && Date.now() < parseInt(expiration)) {
+      // Data is in cache and not expired, use it
+      this.podcasts = JSON.parse(cachedData);
+      console.log("CACHE");
       console.log(this.podcasts);
-      console.log(this.podcasts[0])
-    })
+    } else {
+      // Data is not in cache or expired, get it from the api service to get the 100 popular podcasts and saves it into a variable
+      this.subscription = this.apiService.get100Popular().subscribe((response: any) => {
+        this.podcasts = response.feed.entry;
+        console.log(this.podcasts);
+
+        // Cache the data in localStorage for a day
+        localStorage.setItem('podcastData', JSON.stringify(this.podcasts));
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 1); // Set expiration to one day from now
+        localStorage.setItem('podcastDataExpiration', expirationDate.getTime().toString());
+      });
+    }
   }
 
   get filteredPodcasts(): any[] {
-    if(!this.searchText.trim()){
+    if (!this.searchText.trim()) {
       return this.podcasts;
     }
 
     const lowerCaseSearch = this.searchText.toLowerCase();
-    return this.podcasts.filter((podcast: { [x: string]: { label: string; }; }) => 
+    return this.podcasts.filter((podcast: { [x: string]: { label: string; }; }) =>
       podcast['im:name'].label.toLowerCase().includes(lowerCaseSearch)
       || podcast['im:artist'].label.toLowerCase().includes(lowerCaseSearch))
   }
 
   ngOnDestroy(): void {
     // When the component is destroyed unsubscribes from the previous request
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
