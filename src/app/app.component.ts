6dd8831ff4273f0567bc6bbe78473f32ab213ecd
Oriol Bassols <oriolbn20@gmail.com>
2023-09-03 20:19:47 +0200

    readme and blue dot done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GosbiAngular';
+  isLoading = false;
+  subscription!: Subscription;
+
+  constructor(private router: Router) {
+    this.subscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        // Navigation started, show loading indicator
+        this.isLoading = true;
+      } else if (event instanceof NavigationEnd) {
+        // Navigation ended, hide loading indicator
+        this.isLoading = false;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
